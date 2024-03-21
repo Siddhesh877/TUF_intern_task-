4a76b4e6c1ff6bdf5c91d892307332aac77facd8
Siddhesh Parate <siddheshparate877@gmail.com>
2024-03-21 22:00:39 +0530

    bug fix

--- programming-form-backend/App.js
+++ programming-form-backend/App.js
@@ -37,22 +37,8 @@ cron.schedule("*/180 * * * * *", async function () {
     });
 });
 
-const allowedOrigins = ['http://localhost:3000','https://tuf-intern-task-five.vercel.app'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
-
-
+//allow anyone to access the api
+app.use(cors());
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
