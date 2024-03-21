b093fe67612ebfe981aa12f9b494e2297da01c51
Siddhesh Parate <siddheshparate877@gmail.com>
2024-03-21 14:47:55 +0530

    commented some unwanted changes

--- programming-form-backend/App.js
+++ programming-form-backend/App.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const cron = require('cron');
 
 
+
 const app = express();
 const port = 3001;
 // connectDB();
@@ -15,22 +16,25 @@ table();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-cron.schedule("*/180 * * * * *", async function () {
-  console.log("Restarting server");
+// cron.schedule("*/180 * * * * *", async function () {
+//   console.log("Restarting server");
 
-  await https
-    .get(backendUrl, (res) => {
-      if (res.statusCode === 200) {
-        console.log("Restarted");
-      } else {
-        console.error(`failed to restart with status code: ${res.statusCode}`);
-      }
-    })
-    .on("error", (err) => {
-      console.log("hi");
-      console.error("Error ", err.message);
-    });
-});
+//   await https
+//     .get(backendUrl, (res) => {
+//       if (res.statusCode === 200) {
+//         console.log("Restarted");
+//       } else {
+//         console.error(`failed to restart with status code: ${res.statusCode}`);
+//       }
+//     })
+//     .on("error", (err) => {
+//       console.log("hi");
+//       console.error("Error ", err.message);
+//     });
+// });
+// cron.schedule("*/180 * * * * *", async function () {
+//   console.log("Restarting server");
+// });
 
 const allowedOrigins = ['http://localhost:3000','https://tuf-intern-task-five.vercel.app'];
 app.use(cors({
