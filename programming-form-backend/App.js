b4deb71965b4af65f86e76f9652c9aa16bdcf35d
Siddhesh Parate <siddheshparate877@gmail.com>
2024-03-21 14:44:30 +0530

    implemented cron

--- programming-form-backend/App.js
+++ programming-form-backend/App.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const errorHandler = require('./Middleware/errorHandler');
 const table = require('./models/submissionModel');
 const cors = require('cors');
+const cron = require('cron');
 
 
 const app = express();
@@ -11,8 +12,39 @@ const port = 3001;
 // connectDB();
 table();
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+cron.schedule("*/180 * * * * *", async function () {
+  console.log("Restarting server");
+
+  await https
+    .get(backendUrl, (res) => {
+      if (res.statusCode === 200) {
+        console.log("Restarted");
+      } else {
+        console.error(`failed to restart with status code: ${res.statusCode}`);
+      }
+    })
+    .on("error", (err) => {
+      console.log("hi");
+      console.error("Error ", err.message);
+    });
+});
+
+const allowedOrigins = ['http://localhost:3000','https://tuf-intern-task-five.vercel.app'];
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 
